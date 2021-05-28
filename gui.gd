eac72f7961749d3202fb33a1870cd3229da82a55
Loren Norman <lorennorman@gmail.com>
2021-05-28 00:09:08 -0400

    GUI overhaul

--- gui.gd
+++ gui.gd
@@ -1,35 +1,83 @@
-extends CanvasLayer
+extends Spatial
 
 var hovered_pawn setget set_hovered_pawn
 var hovered_feature setget set_hovered_feature
 var hovered_terrain setget set_hovered_terrain
-var selected_entity setget set_selected_entity
+var selected_entity# setget set_selected_entity
+var selected_location_key
+var selected_cell
+var map
 
 func set_hovered_pawn(pawn):
   var name = pawn.character_name if pawn else ""
-
-  $Left/VSplitContainer/VBoxContainer/PawnHoverLabel.text = "Pawn: %s" % name
+  $Menus/Left/VSplitContainer/VBoxContainer/PawnHoverLabel.text = "Pawn: %s" % name
 
 
 func set_hovered_feature(feature):
   var name = feature.name if feature else ""
+  $Menus/Left/VSplitContainer/VBoxContainer/FeatureHoverLabel.text = "Feature: %s" % name
 
-  $Left/VSplitContainer/VBoxContainer/FeatureHoverLabel.text = "Feature: %s" % name
 
 func set_hovered_terrain(terrain):
   var color = terrain if terrain else Color(0)
+  $Menus/Left/VSplitContainer/VBoxContainer/TerrainHoverLabel/ColorRect.color = color
 
-  $Left/VSplitContainer/VBoxContainer/TerrainHoverLabel/ColorRect.color = color
-
-func set_selected_entity(entity):
-  if entity is Pawn:
-    $Left/VSplitContainer/Panel/MarginContainer/TargetFocus.text = "%s" % entity.character_name
-  else:
-    $Left/VSplitContainer/Panel/MarginContainer/TargetFocus.text = "%s" % entity
 
 ## Jobs
-
 var JobLabel = preload("res://job_label.gd")
 
 func add_job(job):
-  $Right/VSplitContainer/Jobs/JobsList.add_child(JobLabel.new(job))
+  $Menus/Right/VSplitContainer/Jobs/JobsList.add_child(JobLabel.new(job))
+
+func _ready():
+  var _node_rclicked = Events.connect("node_rclicked", self, "handle_cell_rclicked")
+  var _node_lclicked = Events.connect("node_lclicked", self, "handle_cell_lclicked")
+  var _node_hovered = Events.connect("node_hovered", self, "handle_cell_hovered")
+  var _job_added = Events.connect("job_added", self, "add_job")
+
+func handle_cell_lclicked(location_key):
+  if selected_location_key != location_key:
+    selected_location_key = location_key
+
+    selected_cell = map.map_grid.lookup_cell(location_key)
+    $SelectIndicator.translation = selected_cell.position
+
+    # select next entity: pawn, feature, terrain
+    selected_entity = selected_cell.pawn
+    if not selected_entity:
+      selected_entity = selected_cell.feature
+
+    if not selected_entity:
+      selected_entity = selected_cell.terrain
+
+  else:
+    if selected_entity is Color:
+      return
+
+    if selected_entity == selected_cell.pawn and selected_cell.feature:
+      selected_entity = selected_cell.feature
+    else:
+      selected_entity = selected_cell.terrain
+
+  if selected_entity is Pawn:
+    $Menus/Left/VSplitContainer/Panel/MarginContainer/TargetFocus.text = "%s" % selected_entity.character_name
+  else:
+    $Menus/Left/VSplitContainer/Panel/MarginContainer/TargetFocus.text = "%s" % selected_entity
+
+func handle_cell_rclicked(location_key):
+  if selected_entity is Pawn:
+    # sim.make_job(Enums.Jobs.MOVE, location_key, selected_entity)
+    pass
+
+
+var last_hovered
+func handle_cell_hovered(location_key):
+  if location_key != last_hovered:
+    last_hovered = location_key
+    var cell = map.map_grid.lookup_cell(location_key)
+    if cell:
+      self.hovered_pawn = cell.pawn
+      self.hovered_feature = cell.feature
+      self.hovered_terrain = cell.terrain
+
+      $HoverIndicator.translation = cell.position
--- main.gd
+++ main.gd
@@ -8,10 +8,7 @@ var sim
 func _ready():
   var _start_timer = Events.connect("start_timer", self, "handle_start_timer")
   Map.input_camera = $Camera
-  var _node_rclicked = Events.connect("node_rclicked", self, "handle_cell_rclicked")
-  var _node_lclicked = Events.connect("node_lclicked", self, "handle_cell_lclicked")
-  var _node_hovered = Events.connect("node_hovered", self, "handle_cell_hovered")
-  var _job_added = Events.connect("job_added", $GUI, "add_job")
+  $GUI.map = Map
 
   # Create the world with GameState methods
   var game_state = GameState.new()
@@ -43,49 +40,3 @@ func handle_start_timer(timer):
   add_child(timer)
   yield(timer, "timeout")
   remove_child(timer)
-
-var selected_location_key
-var selected_cell
-var selected_entity
-func handle_cell_lclicked(location_key):
-  if selected_location_key != location_key:
-    selected_location_key = location_key
-
-    selected_cell = Map.map_grid.lookup_cell(location_key)
-    $SelectIndicator.translation = selected_cell.position
-
-    # select next entity: pawn, feature, terrain
-    selected_entity = selected_cell.pawn
-    if not selected_entity:
-      selected_entity = selected_cell.feature
-
-    if not selected_entity:
-      selected_entity = selected_cell.terrain
-
-  else:
-    if selected_entity is Color:
-      return
-
-    if selected_entity == selected_cell.pawn and selected_cell.feature:
-      selected_entity = selected_cell.feature
-    else:
-      selected_entity = selected_cell.terrain
-
-  $GUI.selected_entity = selected_entity
-
-func handle_cell_rclicked(location_key):
-  if selected_entity is Pawn:
-    sim.make_job(Enums.Jobs.MOVE, location_key, selected_entity)
-
-
-var last_hovered
-func handle_cell_hovered(location_key):
-  if location_key != last_hovered:
-    last_hovered = location_key
-    var cell = Map.map_grid.lookup_cell(location_key)
-    if cell:
-      $GUI.hovered_pawn = cell.pawn
-      $GUI.hovered_feature = cell.feature
-      $GUI.hovered_terrain = cell.terrain
-
-      $HoverIndicator.translation = cell.position
