9821e335c5822a1b4b414e2df8031416a86c922e
Loren Norman <lorennorman@gmail.com>
2021-06-11 22:10:14 -0400

    add base stats, fix savegame loading

--- game/enums.gd
+++ game/enums.gd
@@ -2,6 +2,8 @@ extends Object
 
 class_name Enums
 
+enum Race { DWARF, ELF, HUMAN, ORC, VOIDBORN }
 enum Jobs { MOVE, BUILD }
 enum Buildings { WALL, DOOR }
 enum Mode { SELECT, BUILD }
+enum Stats { MIGHT, WILL, MAGIC }
--- game/state/pawn.gd
+++ game/state/pawn.gd
@@ -3,8 +3,14 @@ extends Resource
 class_name Pawn
 
 # RPG Stuff
-export(String, "Dwarf", "Elf", "Human") var race = "Dwarf"
+export(String, "Dwarf", "Elf", "Human") var race = "Dwarf" setget set_race
+func set_race(new_race):
+  race = new_race
+  if not stats:
+    stats = PawnStats.by_race(race)
+
 export(String) var character_name setget , _get_name
+export(Resource) var stats = stats as PawnStats
 
 # Real location on map
 export(String) var location = "0,0"
@@ -61,3 +67,29 @@ func start_job_cooldown(time):
   yield(job_timer, "timeout")
   on_cooldown = false
   self.emit_signal("job_cooldown")
+
+class PawnStats:
+  extends Resource
+
+  var might
+  var will
+  var magic
+
+  static func by_race(race):
+    var stats = PawnStats.new()
+
+    match race:
+      Enums.Race.DWARF:
+        stats.might = 1
+        stats.will = 1
+        stats.magic = -1
+      Enums.Race.ELF:
+        stats.might = -1
+        stats.will = 0
+        stats.magic = 2
+      Enums.Race.HUMAN:
+        stats.might = 0
+        stats.will = 2
+        stats.magic = 0
+
+    return stats
