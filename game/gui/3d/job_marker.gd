41f1ed6adb9958de5d575bc20f7460d218c6d2e0
Loren Norman <lorennorman@gmail.com>
2021-06-27 10:34:13 -0400

    make job marker more specific

--- game/gui/3d/job_marker.gd
+++ game/gui/3d/job_marker.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 const BuildingModel = preload("res://game/buildings/building_model.gd")
+
 var job
 var building_model
 
@@ -9,7 +10,7 @@ func calculate_scale(percent): return clamp(percent/100.0, 0.08, 1.0)
 
 
 func _ready():
-  assert(job, "JobMarker became ready without a job")
+  assert(job, "JobMarker became ready without a Job")
   job.connect("updated", self, "_job_updated")
 
   match job.job_type:
@@ -18,12 +19,19 @@ func _ready():
       building_model.cell = job.map_cell
       building_model.scale.y = calculate_scale(job.percent_complete)
       add_child(building_model)
+    Enums.Jobs.HAUL:
+      pass
+    _: printerr("JobMarker created for unknown job_type: %s" % job.job_type)
 
 
 func _job_updated(_updated_job):
-  var target_scale_y = calculate_scale(job.percent_complete)
-  var target_scale = Vector3(building_model.scale.x, target_scale_y, building_model.scale.z)
-  var how_many_seconds = .5 # ask the job how long this should be somehow?
-  $Tween.interpolate_property(building_model, "scale", building_model.scale,
-    target_scale, how_many_seconds, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-  $Tween.start()
+  match job.job_type:
+    Enums.Jobs.BUILD:
+      var target_scale_y = calculate_scale(job.percent_complete)
+      var target_scale = Vector3(building_model.scale.x, target_scale_y, building_model.scale.z)
+      var how_many_seconds = .5 # ask the job how long this should be somehow?
+      $Tween.interpolate_property(building_model, "scale", building_model.scale,
+        target_scale, how_many_seconds, Tween.TRANS_EXPO, Tween.EASE_OUT)
+      $Tween.start()
+    Enums.Jobs.HAUL:
+      pass
