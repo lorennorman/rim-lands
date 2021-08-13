0d2371ac739653035455c5cd703fe0f5a31609e5
Loren Norman <lorennorman@gmail.com>
2021-08-13 17:47:17 -0400

    tree model perturbation

--- game/terrain/trees/tree_model.gd
+++ game/terrain/trees/tree_model.gd
@@ -1,7 +1,14 @@
 extends Spatial
 
 func _ready():
+  var active_tree
   match randi() % 10:
-    1: $tree_simple.visible = true
-    2: $tree_tall.visible = true
-    _: $tree_oak.visible = true
+    1: active_tree = $tree_simple
+    2: active_tree = $tree_tall
+    _: active_tree = $tree_oak
+  active_tree.visible = true
+
+  var rng = RandomNumberGenerator.new()
+  rng.randomize()
+  var translation_offset = Vector3(rng.randf_range(-.5,.5), 0, rng.randf_range(-.5,.5))
+  active_tree.translate(translation_offset)
