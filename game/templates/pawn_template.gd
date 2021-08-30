5b059bf8ff607af83d8013ec1fa03b12c55a2ebc
Loren Norman <lorennorman@gmail.com>
2021-08-30 16:29:21 -0400

    rough in the pawn template and generation

--- game/templates/pawn_template.gd
+++ game/templates/pawn_template.gd
@@ -1,2 +1,11 @@
 extends Resource
 class_name PawnTemplate
+
+export(String) var name = ""
+export(bool) var use_random_name = false
+
+export(String, "Hum", "Dorv", "Alv", "Erk") var race
+export(bool) var use_random_race = false
+
+export(String) var location
+export(bool) var use_random_location = false
