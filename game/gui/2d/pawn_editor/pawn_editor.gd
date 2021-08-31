38adabfc183d15570a2acee8f0963730411b12aa
Loren Norman <lorennorman@gmail.com>
2021-08-31 19:39:09 -0400

    add reroll button to pawn editor, aspirational

--- game/gui/2d/pawn_editor/pawn_editor.gd
+++ game/gui/2d/pawn_editor/pawn_editor.gd
@@ -1,9 +1,16 @@
 tool
 extends Control
 
+
 export(Resource) var pawn: Resource setget set_pawn
 func set_pawn(new_pawn: Pawn):
   pawn = new_pawn
+  $Reroll.visible = true
   $Name.text = pawn.character_name
   $Race.text = pawn.race
   $Stats.text = pawn.location
+
+
+func reroll_pressed():
+  if pawn:
+    print("Rerolling %s..." % pawn.character_name)
