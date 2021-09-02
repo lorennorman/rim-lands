bb30db5558a1a3b8c437cfa8ad2ff8f8f8a9e673
Loren Norman <lorennorman@gmail.com>
2021-09-02 12:59:12 -0400

    make a gdscript test suite to check things for myself!

---
+++ test/gdscript_suite.gd
@@ -0,0 +1,45 @@
+extends "./test.gd"
+
+func test_array_comparison():
+  expect(not [], "empty arrays are false")
+  expect([1], "non-empty arrays are true")
+  expect([] == [], "empty arrays should be equal")
+  expect([1, 2, 3] == [1, 2, 3], "arrays are compared by their contents")
+
+
+func test_resource_with_initialized_arrays():
+  var res1 = ResourceWithInitializedProperties.new()
+  var res2 = ResourceWithInitializedProperties.new()
+
+  expect(res1 != res2, "Resource instances are different.")
+
+  expect(res2.arr.size() == 0, "Arrays start empty")
+  res1.arr.push_back(1) # modify res1's array, then check res2's array for the change
+  expect(res2.arr[0] == 1, "Resources that initialize share the same arrays")
+
+  var res3 = ResourceWithInitializedProperties.new()
+  expect(res1.arr == res3.arr, "Even Resources created later")
+
+
+func test_resource_without_initialized_arrays():
+  var res1 = ResourceWithoutInitializedProperties.new()
+  var res2 = ResourceWithoutInitializedProperties.new()
+
+  expect(res1.arr == [], "Arrays get initialized automatically")
+  expect(res1.arr == res2.arr, "Initialized arrays match")
+  res1.arr.push_back(1) # modify res1's array, then check res2's array for the change
+  expect(res1.arr != res2.arr, "One array changed, arrays no longer match")
+
+
+class ResourceWithInitializedProperties:
+  extends Resource
+
+  export(Resource) var res = Resource.new()
+  export(Array, Resource) var arr = []
+
+
+class ResourceWithoutInitializedProperties:
+  extends Resource
+
+  export(Resource) var res
+  export(Array, Resource) var arr
--- test/test_label.gd
+++ test/test_label.gd
@@ -3,7 +3,8 @@ extends Label
 var test_method_name
 var test
 
-const label_template = "[%s] %s "
+const label_template = "[%s] %s"
+const failure_template = label_template + "\n     REASON: %s"
 
 func _init(new_test_method_name, new_test).():
   test_method_name = new_test_method_name
@@ -15,6 +16,6 @@ func run():
   test.call(test_method_name)
 
   if test.failure_reason:
-    text = (label_template % ["X", test_method_name]) + test.failure_reason
+    text = failure_template % ["X", test_method_name, test.failure_reason]
   else:
     text = label_template % [".", test_method_name]
