17f6a12cb70da77a4a4339688d6ee517c3bfa6ae
Loren Norman <lorennorman@gmail.com>
2021-09-30 16:54:32 -0400

    game state is only data, all action in game_store

--- game/store/game_store.gd
+++ game/store/game_store.gd
@@ -68,6 +68,7 @@ func get_forests():
   return game_state.map_grid.forests
 
 
+### Pawns ###
 func add_pawn(new_pawn: Pawn):
   # pass
   # gather resources
@@ -80,15 +81,110 @@ func add_pawn(new_pawn: Pawn):
 
 
 func destroy_pawn(pawn, erase=true):
-  if erase: pawns.erase(pawn)
+  if erase: game_state.pawns.erase(pawn)
   StateActivator.deactivate_pawn(pawn, map)
   emit_signal("pawn_removed", pawn)
 
 
+### Jobs ###
 func add_job(job: Job):
   game_state.jobs.push_back(job)
   StateActivator.activate_job(job, map)
   emit_signal("job_added", job)
+
+
+### Buildings ###
+func add_building(building):
+  game_state.buildings.push_back(building)
+  StateActivator.activate_building(building, map)
+  emit_signal("building_added", building)
+
+
+### Items ###
+func add_item(item):
+  game_state.items.push_back(item)
+  StateActivator.activate_item(item, map)
+  emit_signal("item_added", item)
+
+
+## Jobs
+func complete_job(job, erase=true):
+  match job.job_type:
+    Enums.Jobs.BUILD:
+      add_building(Building.new({
+        type = job.building_type,
+        location = job.location
+      }))
+
+  if erase: jobs.erase(job)
+  job.removed = true
+  Events.emit_signal("job_removed", job)
+
+
+## Forests
+func buildup_forest(forest):
+  StateActivator.activate_forest(forest, map)
+
+
+func destroy_forest(forest_dict):
+  var found_forest
+
+  for forest in self.forests:
+    if forest.x == forest_dict.x and forest.z == forest_dict.z:
+      found_forest = forest
+      break
+
+  if found_forest:
+    self.forests.erase(found_forest)
+    emit_signal("forest_removed", found_forest)
+
+  else:
+    printerr("Didn't find a matching forest for %s" % forest_dict)
+
+
+## Items
+func destroy_item(item):
+  items.erase(item)
+  StateActivator.deactivate_item(item)
+  Events.emit_signal("item_removed", item)
+
+
+func find_closest_available_material_to(material_type, origin_cell: MapCell):
+  var closest_item = null
+  var distance = 100_000
+  for item in items:
+    if item.type != material_type or item.is_claimed() or not item.is_on_map(): continue
+
+    var item_distance = map.get_move_path(origin_cell, item.map_cell).size()
+    if item_distance < distance:
+      closest_item = item
+      distance = item_distance
+
+  return closest_item
+
+
+func transfer_item_from_to(item_type: int, item_quantity: int, from: Resource, to: Resource):
+  # check that "from" has enough of this item
+  var from_item = from.get_item(item_type)
+  assert(from_item, "Attempted to transfer an item the source doesn't have: %s" % item_type)
+  assert(from_item.quantity >= item_quantity, "Attempted to transfer more of an item than the source has: %s < %s" % [from_item.quantity, item_quantity])
+  from_item.quantity -= item_quantity
+  if from_item.quantity <= 0: destroy_item(from_item)
+
+  # check if "to" already has some of this item
+  var to_item = to.get_item(item_type)
+  if to_item:
+    to_item.quantity += item_quantity
+  else:
+    # do the new
+    to_item = Item.new({
+      "type": item_type,
+      "quantity": item_quantity,
+      "owner": to
+    })
+    add_item(to_item)
+
+
 
 class Map:
   var astar := AStar.new()
